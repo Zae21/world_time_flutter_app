6bf7f009e5bde7995876a1295c118ef00cc7114a
zae21 <prayoga96@gmail.com>
2021-02-16 23:55:49 +0700

    add new sections

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:world_time_flutter_app/services/world-time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -9,21 +8,15 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void loadTime() async {
-    Response resp = await get('http://worldtimeapi.org/api/timezone/Asia/Jakarta');
-    Map data = jsonDecode(resp.body);
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
-
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print(now);
+  void SetupWorldTime(){
+    WorldTime instance = WorldTime(location:'Jakarta',flag:'indonesia.png',url:'Asia/Jakarta');
+    instance.loadTime();
   }
 
   @override
   void initState() {
     super.initState();
-    loadTime();
+    SetupWorldTime();
   }
 
   @override
